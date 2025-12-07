6266ad6aacde8d8f123fc26a023fc2accbef676c
workfatima829 <workfatima829@gmail.com>
2025-12-07 21:29:44 +0500

    Implement user login and generate JWT token

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,7 +4,10 @@ import usersDetails from "../data/usersDetails.json"
 import validateUser from "../middlewares/userMiddleware";
 import { v4 as uuidv4 } from "uuid";
 import { Router } from "express";
-import bycrypt from "bcrypt";
+import bcrypt from "bcrypt";
+import  jwt  from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 const router = Router();
 
 interface User {
@@ -19,7 +22,7 @@ const usersDetailsTyped: User[] = usersDetails as User[];
 router.post('/userSignup', validateUser, (req, res) => {
     const { name, email, password } = req.body;
     const saltRounds = 10;
-    bycrypt.hash(password, saltRounds, (err, hashedPassword) => {
+    bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
         if (err) {
             return res.status(500).json({ error: "Error hashing password" });
         }
@@ -41,6 +44,27 @@ router.post('/userSignup', validateUser, (req, res) => {
     });
 });
 
-
+router.post('/userLogin', (req, res) => {
+    const { email, password } = req.body;
+    const user = usersDetailsTyped.find(u => u.email === email);
+    if (!user) {
+        return res.status(401).send({ message: "User not found" });
+    }
+     bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) {
+            return res.status(500).send({ message: "Error comparing passwords" });
+        }
+        if (!isMatch) {
+            return res.status(401).send({ message: "Invalid password" });
+        }
+        const token = jwt.sign(
+            {id: user.id, email: user.email},
+            process.env.JWT_SECRET as string,
+            { expiresIn: "7d" }
+        )
+      
+        return res.status(200).send({ message: "Login successful" , token});
+    });
+});
 
 export default router;
